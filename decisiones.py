3fe3f3e79b10de532b1b0ade55d726d8e65cd94e
Eduardo Dguez <renixedu@gmail.com>
2026-07-27 16:05:03 -0600

    feat: estructuras condicionales - if, elif, else

--- decisiones.py
+++ decisiones.py
@@ -4,3 +4,17 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
+
+
+    edad = 25
+
+edad = 10  # Cambia esto a: 15, 25, 65 y ejecuta cada vez
+
+if edad < 13:
+    print("Eres un niño")
+elif edad < 20:
+    print("Eres un adolescente")
+elif edad < 60:
+    print("Eres un adulto")
+else:
+    print("Eres un adulto mayor")
