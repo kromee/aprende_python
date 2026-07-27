2dd00f276a3907ad7499a027e7727ab3c5cbacc5
Eduardo Dguez <renixedu@gmail.com>
2026-07-27 16:10:57 -0600

    feat: bucles for - iteracion sobre listas

---
+++ bucles.py
@@ -0,0 +1,10 @@
+frutas = ["manzana", "pera", "uva", "naranja"]
+
+for fruta in frutas:
+    print(fruta)
+
+
+    nombres = ["Ana", "Luis", "Carlos"]
+
+for nombre in nombres:
+    print(f"Hola, {nombre}")
