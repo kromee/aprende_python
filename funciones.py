6f070aaab2b1d6522f13725d99dc4713b4c73797
Eduardo Dguez <renixedu@gmail.com>
2026-07-27 18:48:46 -0600

    feat: funciones con def y return

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def saludar(nombre):
+    print(f"Hola, {nombre}")
+
+saludar("Ana")
+saludar("Luis")
+
+
+
+def doble(numero):
+    return numero * 2
+
+# Pruebas:
+print(doble(5))    # Debe imprimir: 10
+print(doble(7))    # Debe imprimir: 14
+print(doble(3.5))  # Debe imprimir: 7.0
