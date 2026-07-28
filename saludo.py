76e64384cf4bc7590bea832f5c6e252adddbd799
Eduardo Dguez <renixedu@gmail.com>
2026-07-27 18:55:25 -0600

    feat: parametros por defecto en funciones

---
+++ saludo.py
@@ -0,0 +1,6 @@
+def presentar(nombre, edad=18):
+    print(f"Me llamo {nombre} y tengo {edad} años")
+
+# Pruebas:
+presentar("Carlos")        # Debe dar: Me llamo Carlos y tengo 18 años
+presentar("Ana", 25)       # Debe dar: Me llamo Ana y tengo 25 años
